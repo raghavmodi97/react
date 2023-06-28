d859a44fba1a8806268c6ecddb05a87de6c3588a
Raghav Modi <raghavmodi1997@gmail.com>
2023-06-28 20:30:41 +0530

    ChildProps

--- src/index.js
+++ src/index.js
@@ -17,43 +17,38 @@ const Book2 = {
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={Book1.author} title={Book1.title} image={Book1.image} />
-      <Book author={Book2.author} title={Book2.title} image={Book2.image} />
+      <Book author={Book1.author} title={Book1.title} image={Book1.image}>
+        <button>Click Me</button>
+      </Book>
+      <Book author={Book2.author} title={Book2.title} image={Book2.image}>
+        <p>Clilck Me</p>
+      </Book>
     </section>
   );
 };
 
 const Book = (props) => {
-  const { image, title, author } = props;
+  const { image, title, author, children } = props;
   return (
     <article className="book">
-      {/* <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author} </h4> */}
-      {/* define image title authore above in props
-      to access without using props.image etc. */}
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
 // another way to write above code
-// const Book = ({ image, title, author } ) => {
-//
+// const Book = ({ image, title, author, children }) => {
 //   return (
 //     <article className="book">
-//       {/* <img src={props.image} alt={props.title} />
-//       <h2>{props.title}</h2>
-//       <h4>{props.author} </h4> */}
-//       {/* define image title authore above in props
-//       to access without using props.image etc. */}
 //       <img src={image} alt={title} />
 //       <h2>{title}</h2>
 //       <h4>{author}</h4>
+//       {children}
 //     </article>
 //   );
 // };
-// }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
