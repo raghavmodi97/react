bc963b048fd91e2a480c8af5bf73f2527579e9d7
Raghav Modi <raghavmodi1997@gmail.com>
2023-07-06 19:37:38 +0530

    ImageInSrcFolder

--- src/books.js
+++ src/books.js
@@ -1,14 +1,23 @@
+import img1 from "./Images/Book_1.jpg";
+import img2 from "./Images/Book_2.jpg";
+import img3 from "./Images/Book_3.jpg";
 export const books = [
   {
     title: "Iron Flame",
     author: "Rebecca Yarros",
-    image: "./Images/Book_1.jpg",
+    image: img1,
     id: 1,
   },
   {
     title: "The Covenant Of War",
     author: "Abraham Verghese",
-    image: "./Images/Book_2.jpg",
+    image: img2,
     id: 2,
+  },
+  {
+    title: "The Housemaid",
+    author: "Freida McFadden",
+    image: img3,
+    id: 3,
   },
 ];
